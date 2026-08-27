9b153d5616de289dac9e307d48f1f66e0ceb34fa
acn-ariel <ariel.s.udtohan@accenture.com>
2026-08-27 16:02:42 +0800

    update datastream

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -168,7 +168,7 @@ async function getAndApplyRenderDecisions() {
 
 // Initialise immediately — promise is awaited in loadEager
 const alloyLoadedPromise = initWebSDK('./alloy.js', {
-  datastreamId: 'd899f130-bab1-43e9-9646-56d2d336d665',
+  datastreamId: 'd7e718aa-3cf8-429f-bc60-9921cdbed6cc',
   orgId: '0CEB60F754C7E06B0A4C98A2@AdobeOrg',
 });
 
