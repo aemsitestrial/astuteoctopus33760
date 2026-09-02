dcb0fec8f333fd13567875bdfc1cb169853021cb
acn-ariel <ariel.s.udtohan@accenture.com>
2026-09-02 11:28:33 +0800

    remove edgeConfigOverrides from getAndApplyRenderDecisions function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -144,12 +144,7 @@ async function getAndApplyRenderDecisions() {
     renderDecisions: false,
     personalization: {
       decisionScopes: ['hero'],
-    },
-    edgeConfigOverrides: {
-      com_adobe_target: {
-        propertyToken: '7a4abb00-3faa-2295-981b-ad9e07673135',
-      },
-    },
+    }
   });
   const { propositions } = response;
 
