7cf366e25ac3909605dad4b02ae12bcab5360230
acn-ariel <ariel.s.udtohan@accenture.com>
2026-09-03 08:01:15 +0800

    fix: add missing comma in getAndApplyRenderDecisions function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -144,7 +144,7 @@ const JSON_CONTENT_SCHEMA = 'https://ns.adobe.com/personalization/json-content-i
 
 async function getAndApplyRenderDecisions() {
   const response = await window.alloy('sendEvent', {
-    renderDecisions: false
+    renderDecisions: false,
   });
   const { propositions } = response;
 
